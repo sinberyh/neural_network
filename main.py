cefa14bf95dd37cb15a2e86b0fe5d22a7b5d4dbd
sinberyh <592582598@qq.com>
2017-06-14 11:14:07 +0800

    add asyncio

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
+import asyncio
 from todo.student import Student
 
+
 def hello():
-    s=Student('yjl')
+    s = Student('yjl')
     print(s.name)
     print("hello world.")
     print("changed https to ssh")
 
+
+
+async def wget(host):
+    print('wget %s...' % host)
+    connect = asyncio.open_connection(host, 80)
+    reader, writer = await connect
+    header = 'GET / HTTP/1.0\r\nHost: %s\r\n\r\n' % host
+    writer.write(header.encode('utf-8'))
+    await writer.drain()
+    while True:
+        line = await reader.readline()
+        if line == b'\r\n':
+            break
+        print('%s header > %s' % (host, line.decode('utf-8').rstrip()))
+    # Ignore the body, close the socket
+    writer.close()
+
+
 def main():
-    hello()
+    loop = asyncio.get_event_loop()
+    tasks = [wget(host)
+             for host in ['www.sina.com.cn', 'www.sohu.com', 'www.163.com']]
+    loop.run_until_complete(asyncio.wait(tasks))
+    loop.close()
 
 
 if __name__ == '__main__':
     main()
-
-
