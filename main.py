0852d621288bcd3c51e85d93b3c811e2a38d9bed
sinberyh <592582598@qq.com>
2017-06-15 15:36:23 +0800

    unit test fix

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from todo.student import Student
 
+
 def hello():
-    s=Student('yjl')
+    s = Student('yjl')
     print(s.name)
     print("hello world.")
     print("changed https to ssh")
+
 
 def main():
     hello()
@@ -12,5 +14,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
--- todo/tests/test_student.py
+++ todo/tests/test_student.py
@@ -1,7 +1,8 @@
+# from ..student import Student
 from todo.student import Student
+
 
 class TestStudent:
     def test_name(self):
         s = Student('yjl')
         assert s.name == 'yjl'
-
